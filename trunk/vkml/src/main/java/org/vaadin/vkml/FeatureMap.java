95423385083c04b68c0525eeab37b758b1e86f32
matti.tahvonen@gmail.com <matti.tahvonen@gmail.com@e84859ac-0a27-f48f-5136-a6b5d5a7b96c>
2011-08-26 13:21:39 +0000

    Styles are now really visualized on the map. Previously styles only in KML file (visualized in GEarth).

--- trunk/vkml/src/main/java/org/vaadin/vkml/FeatureMap.java
+++ trunk/vkml/src/main/java/org/vaadin/vkml/FeatureMap.java
@@ -1,7 +1,6 @@
 package org.vaadin.vkml;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,6 +12,7 @@ import org.vaadin.vol.GoogleStreetMapLayer;
 import org.vaadin.vol.MapTilerLayer;
 import org.vaadin.vol.OpenLayersMap;
 import org.vaadin.vol.Point;
+import org.vaadin.vol.StyleMap;
 import org.vaadin.vol.Vector;
 import org.vaadin.vol.VectorLayer;
 import org.vaadin.vol.VectorLayer.DrawingMode;
@@ -37,192 +37,201 @@ import de.micromata.opengis.kml.v_2_2_0.Placemark;
 import de.micromata.opengis.kml.v_2_2_0.Polygon;
 
 public class FeatureMap extends OpenLayersMap implements VectorDrawnListener,
-		VectorModifiedListener, VectorSelectedListener {
-	static final double METERS_ABOVE_GROUND = 30;
-	private VectorLayer vectorLayer;
-	private FeatureDrawnCallback drawingListener;
-	private DocumentView documentView;
+        VectorModifiedListener, VectorSelectedListener {
+    static final double METERS_ABOVE_GROUND = 30;
+    private VectorLayer vectorLayer;
+    private FeatureDrawnCallback drawingListener;
+    private DocumentView documentView;
 
-	public FeatureMap(DocumentView documentView) {
-		this.documentView = documentView;
-		addBaseLayers();
-		vectorLayer = new VectorLayer();
-		vectorLayer.addListener((VectorDrawnListener) this);
-		vectorLayer.setDrawindMode(DrawingMode.MODIFY);
-		addLayer(vectorLayer);
-		setSizeFull();
-		setDefaultCenterAndZoom();
-		vectorLayer.addListener((VectorModifiedListener) this);
-		vectorLayer.addListener((VectorSelectedListener) this);
-	}
+    public FeatureMap(DocumentView documentView) {
+        this.documentView = documentView;
+        addBaseLayers();
+        vectorLayer = new VectorLayer();
+        vectorLayer.addListener((VectorDrawnListener) this);
+        vectorLayer.setDrawindMode(DrawingMode.MODIFY);
+        addLayer(vectorLayer);
+        setSizeFull();
+        setDefaultCenterAndZoom();
+        vectorLayer.addListener((VectorModifiedListener) this);
+        vectorLayer.addListener((VectorSelectedListener) this);
+    }
 
-	protected void setDefaultCenterAndZoom() {
-		setCenter(22.805, 60.447);
-		setZoom(15);
-	}
+    protected void setDefaultCenterAndZoom() {
+        setCenter(22.805, 60.447);
+        setZoom(15);
+    }
 
-	protected void addBaseLayers() {
-		GoogleStreetMapLayer googleStreets = new GoogleStreetMapLayer();
-		addLayer(googleStreets);
-		try {
-			// virtuallypreinstalled don't support downloading resources form it
-			// self?
-			// MapTilerLayer mapTilerLayer = new MapTilerLayer(
-			// "http://matti.virtuallypreinstalled.com/tiles/pirttikankare/pirttikankare/");
-//			MapTilerLayer mapTilerLayer = new MapTilerLayer(
-//					"http://localhost:9999/VAADIN/perus_kiint_2m/");
-//			mapTilerLayer.setDisplayName("Peruskartta");
-//			mapTilerLayer.setBaseLayer(false);
-//			addLayer(mapTilerLayer);
-			MapTilerLayer mapTilerLayer= new MapTilerLayer(
-					"http://dl.dropbox.com/u/4041822/pirttikankare/");
-			mapTilerLayer.setDisplayName("Pirttikankare");
-			mapTilerLayer.setBaseLayer(false);
-			addLayer(mapTilerLayer);
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    protected void addBaseLayers() {
+        GoogleStreetMapLayer googleStreets = new GoogleStreetMapLayer();
+        addLayer(googleStreets);
+        try {
+            // virtuallypreinstalled don't support downloading resources form it
+            // self?
+            // MapTilerLayer mapTilerLayer = new MapTilerLayer(
+            // "http://matti.virtuallypreinstalled.com/tiles/pirttikankare/pirttikankare/");
+            // MapTilerLayer mapTilerLayer = new MapTilerLayer(
+            // "http://localhost:9999/VAADIN/perus_kiint_2m/");
+            // mapTilerLayer.setDisplayName("Peruskartta");
+            // mapTilerLayer.setBaseLayer(false);
+            // addLayer(mapTilerLayer);
+            MapTilerLayer mapTilerLayer = new MapTilerLayer(
+                    "http://dl.dropbox.com/u/4041822/pirttikankare/");
+            mapTilerLayer.setDisplayName("Pirttikankare");
+            mapTilerLayer.setBaseLayer(false);
+            addLayer(mapTilerLayer);
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public void drawFeature(final FeatureDrawnCallback listener) {
-		if (drawingListener != null) {
-			cancelDrawing();
-		}
-		drawingListener = listener;
+    public void drawFeature(final FeatureDrawnCallback listener) {
+        if (drawingListener != null) {
+            cancelDrawing();
+        }
+        drawingListener = listener;
 
-		vectorLayer.removeAllComponents();
-		vectorLayer.setDrawindMode(DrawingMode.AREA);
+        vectorLayer.removeAllComponents();
+        vectorLayer.setDrawindMode(DrawingMode.AREA);
 
-	}
+    }
 
-	private void cancelDrawing() {
-		vectorLayer.setDrawindMode(DrawingMode.MODIFY);
-		drawingListener = null;
-	}
+    private void cancelDrawing() {
+        vectorLayer.setDrawindMode(DrawingMode.MODIFY);
+        drawingListener = null;
+    }
 
-	public void vectorDrawn(VectorDrawnEvent event) {
-		drawingListener.drawingDone(event.getVector());
-		vectorLayer.setDrawindMode(DrawingMode.MODIFY);
-	}
+    public void vectorDrawn(VectorDrawnEvent event) {
+        drawingListener.drawingDone(event.getVector());
+        vectorLayer.setDrawindMode(DrawingMode.MODIFY);
+    }
 
-	public void showFeature(Feature value) {
-		if (value instanceof Folder) {
-			List<Feature> features = ((Folder) value).getFeature();
-			for (Feature feature : features) {
-				showFeature(feature);
-			}
-		} else if (value instanceof Document) {
-			List<Feature> features = ((Document) value).getFeature();
-			for (Feature feature : features) {
-				showFeature(feature);
-			}
-		} else if (value instanceof Placemark) {
-			Placemark pm = (Placemark) value;
-			Geometry geometry = pm.getGeometry();
-			showFeature(geometry);
-		} else {
-			System.err.println("Unhandled feature type.");
-		}
+    public void showFeature(Feature value) {
+        if (value instanceof Folder) {
+            List<Feature> features = ((Folder) value).getFeature();
+            for (Feature feature : features) {
+                showFeature(feature);
+            }
+        } else if (value instanceof Document) {
+            List<Feature> features = ((Document) value).getFeature();
+            for (Feature feature : features) {
+                showFeature(feature);
+            }
+        } else if (value instanceof Placemark) {
+            Placemark pm = (Placemark) value;
+            Geometry geometry = pm.getGeometry();
+            showFeature(geometry, pm.getStyleUrl());
+        } else {
+            System.err.println("Unhandled feature type.");
+        }
 
-	}
-	
-	public boolean isDrawn(Polygon p) {
-		return getVectorFor(p) != null;
-	}
-	
-	private Vector getVectorFor(Geometry g) {
-		Iterator<Component> componentIterator = vectorLayer.getComponentIterator();
-		while(componentIterator.hasNext()) {
-			AbstractComponent next = (AbstractComponent) componentIterator.next();
-			if(next.getData() == g) {
-				return (Vector) next;
-			}
-		}
-		return null;
-	}
+    }
 
-	public void showFeature(Polygon p) {
-		if(isDrawn(p)) {
-			return;
-		}
-		List<Coordinate> coordinates = ((Polygon) p).getOuterBoundaryIs()
-				.getLinearRing().getCoordinates();
-		Point[] points = new Point[coordinates.size()];
-		int i = 0;
-		for (Coordinate coordinate : coordinates) {
-			Point point = new Point(coordinate.getLongitude(),
-					coordinate.getLatitude());
-			points[i++] = point;
-		}
+    public boolean isDrawn(Polygon p) {
+        return getVectorFor(p) != null;
+    }
 
-		if (points.length > 0) {
-			Area area = new Area();
-			area.setPoints(points);
-			area.setData(p);
-			vectorLayer.addComponent(area);
-		}
-		vectorLayer.setDrawindMode(DrawingMode.MODIFY);
-	}
+    public Vector getVectorFor(Geometry g) {
+        Iterator<Component> componentIterator = vectorLayer
+                .getComponentIterator();
+        while (componentIterator.hasNext()) {
+            AbstractComponent next = (AbstractComponent) componentIterator
+                    .next();
+            if (next.getData() == g) {
+                return (Vector) next;
+            }
+        }
+        return null;
+    }
 
-	public void showFeature(Geometry p) {
-		if (p instanceof Polygon) {
-			showFeature((Polygon) p);
-		}
+    public void showFeature(Polygon p, String styleUrl) {
+        if (isDrawn(p)) {
+            return;
+        }
+        List<Coordinate> coordinates = (p).getOuterBoundaryIs().getLinearRing()
+                .getCoordinates();
+        Point[] points = new Point[coordinates.size()];
+        int i = 0;
+        for (Coordinate coordinate : coordinates) {
+            Point point = new Point(coordinate.getLongitude(),
+                    coordinate.getLatitude());
+            points[i++] = point;
+        }
 
-	}
+        if (points.length > 0) {
+            Area area = new Area();
+            area.setPoints(points);
+            area.setData(p);
+            area.setRenderIntent(styleUrl);
+            vectorLayer.addComponent(area);
+        }
+        vectorLayer.setDrawindMode(DrawingMode.MODIFY);
+    }
 
-	public void clear() {
-		vectorLayer.removeAllComponents();
-		vectorLayer.requestRepaint();
-	}
+    public void showFeature(Geometry p, String styleUrl) {
+        if (p instanceof Polygon) {
+            showFeature((Polygon) p, styleUrl);
+        }
 
-	public void vectorModified(VectorModifiedEvent event) {
-		Vector vector = event.getVector();
-			if(vector instanceof Area) {
-				/*
-				 * An area has been modified, update data to backing model object.
-				 */
-				Point[] points = ((Area)vector).getPoints();
-				Polygon data = (Polygon) vector.getData();
-				LinearRing linearRing = data.getOuterBoundaryIs().getLinearRing();
-				List<Coordinate> coordinates2 = linearRing.getCoordinates();
-				coordinates2.clear();
-				for (Point p : points) {
-					coordinates2.add(new Coordinate(p.getLon(), p.getLat(), METERS_ABOVE_GROUND));
-				}
-			}
-	}
+    }
 
-	public void vectorSelected(VectorSelectedEvent event) {
-		Vector vector = event.getVector();
-		documentView.selectFeatureByGeometry((Geometry) vector.getData());
-	}
+    public void clear() {
+        vectorLayer.removeAllComponents();
+        vectorLayer.requestRepaint();
+    }
 
-	public void showAllVectors() {
-		Iterator<Component> componentIterator = vectorLayer.getComponentIterator();
-		Bounds bounds = new Bounds();
-		boolean emptyBounds = true;
-		while(componentIterator.hasNext()) {
-			Vector vector = (Vector) componentIterator.next();
-			bounds.extend(vector.getPoints());
-			if(emptyBounds) {
-				if(vector.getPoints().length < 2) {
-					bounds.extend(vector.getPoints());
-				}
-				emptyBounds = false;
-			}
-		}
-		if(!emptyBounds) {
-			zoomToExtent(bounds);
-		}
-		
-	}
+    public void vectorModified(VectorModifiedEvent event) {
+        Vector vector = event.getVector();
+        if (vector instanceof Area) {
+            /*
+             * An area has been modified, update data to backing model object.
+             */
+            Point[] points = ((Area) vector).getPoints();
+            Polygon data = (Polygon) vector.getData();
+            LinearRing linearRing = data.getOuterBoundaryIs().getLinearRing();
+            List<Coordinate> coordinates2 = linearRing.getCoordinates();
+            coordinates2.clear();
+            for (Point p : points) {
+                coordinates2.add(new Coordinate(p.getLon(), p.getLat(),
+                        METERS_ABOVE_GROUND));
+            }
+        }
+    }
+
+    public void vectorSelected(VectorSelectedEvent event) {
+        Vector vector = event.getVector();
+        documentView.selectFeatureByGeometry((Geometry) vector.getData());
+    }
+
+    public void showAllVectors() {
+        Iterator<Component> componentIterator = vectorLayer
+                .getComponentIterator();
+        Bounds bounds = new Bounds();
+        boolean emptyBounds = true;
+        while (componentIterator.hasNext()) {
+            Vector vector = (Vector) componentIterator.next();
+            bounds.extend(vector.getPoints());
+            if (emptyBounds) {
+                if (vector.getPoints().length < 2) {
+                    bounds.extend(vector.getPoints());
+                }
+                emptyBounds = false;
+            }
+        }
+        if (!emptyBounds) {
+            zoomToExtent(bounds);
+        }
+
+    }
+
+    public void setStyleMap(StyleMap styleMap) {
+        vectorLayer.setStyleMap(styleMap);
+    }
 
 }
--- trunk/vkml/src/main/java/org/vaadin/vkml/GeometryEditor.java
+++ trunk/vkml/src/main/java/org/vaadin/vkml/GeometryEditor.java
@@ -20,97 +20,97 @@ import de.micromata.opengis.kml.v_2_2_0.LinearRing;
 import de.micromata.opengis.kml.v_2_2_0.Polygon;
 
 public class GeometryEditor extends CssLayout implements ClickListener,
-		FeatureDrawnCallback {
+        FeatureDrawnCallback {
 
-	private Button draw;
-	private Label label;
-	private List<Coordinate> coordinates;
-	private FeatureMap map;
-	private Polygon polygon;
+    private Button draw;
+    private Label label;
+    private List<Coordinate> coordinates;
+    private FeatureMap map;
+    private Polygon polygon;
 
-	public GeometryEditor(Geometry geometry, DocumentView owner) {
-		map = owner.getMap();
-		setCaption("Geometry");
-		label = new Label();
-		addComponent(label);
-		draw = new Button("Redraw", this);
-		addComponent(draw);
+    public GeometryEditor(Geometry geometry, DocumentView owner) {
+        map = owner.getMap();
+        setCaption("Geometry");
+        label = new Label();
+        addComponent(label);
+        draw = new Button("Redraw", this);
+        addComponent(draw);
 
-		if (geometry instanceof Polygon) {
-			polygon = (Polygon) geometry;
-			Boundary outerBoundary = polygon.getOuterBoundaryIs();
-			LinearRing ring = outerBoundary.getLinearRing();
-			coordinates = ring.getCoordinates();
+        if (geometry instanceof Polygon) {
+            polygon = (Polygon) geometry;
+            Boundary outerBoundary = polygon.getOuterBoundaryIs();
+            LinearRing ring = outerBoundary.getLinearRing();
+            coordinates = ring.getCoordinates();
 
-			if (!ring.getCoordinates().isEmpty()) {
-				updateCoordinateLabel();
-				Bounds bounds = new Bounds();
-				for(Coordinate c : coordinates) {
-					bounds.extend(new Point(c.getLongitude(),c.getLatitude()));
-				}
-				owner.getMap().zoomToExtent(bounds);
-				
-				map.showFeature(polygon);
-			} else {
-				// new component, draw mode by default
-				drawFeature();
-			}
+            if (!ring.getCoordinates().isEmpty()) {
+                updateCoordinateLabel();
+                Bounds bounds = new Bounds();
+                for (Coordinate c : coordinates) {
+                    bounds.extend(new Point(c.getLongitude(), c.getLatitude()));
+                }
+                owner.getMap().zoomToExtent(bounds);
 
-		} else {
-			addComponent(new Label("Geometry type not supported"
-					+ geometry.getClass()));
-		}
-	}
+                map.showFeature(polygon, null);
+            } else {
+                // new component, draw mode by default
+                drawFeature();
+            }
 
-	private void updateCoordinateLabel() {
-		label.setValue(coordinates.size() + " points.");
-	}
+        } else {
+            addComponent(new Label("Geometry type not supported"
+                    + geometry.getClass()));
+        }
+    }
 
-	public void buttonClick(ClickEvent event) {
-		if (event.getButton() == draw) {
-			drawFeature();
-		}
-	}
+    private void updateCoordinateLabel() {
+        label.setValue(coordinates.size() + " points.");
+    }
 
-	private void drawFeature() {
-		map.drawFeature(this);
-		draw.setVisible(false);
-		label.setValue("Drawing...");
-	}
+    public void buttonClick(ClickEvent event) {
+        if (event.getButton() == draw) {
+            drawFeature();
+        }
+    }
 
-	public void drawingDone(Vector vector) {
-		if (vector instanceof Area) {
-			Area area = (Area) vector;
-			coordinates.clear();
-			Point[] points = area.getPoints();
-			for (Point point : points) {
-				/*
-				 * Note that we intentionally place overlays 30 meter above
-				 * ground. This way above base layers in google earth (probably
-				 * bugs if there are lots of altitude differences and only few
-				 * points in the area (or large area).
-				 * 
-				 * TODO check whats up with clamped to ground option. Last time
-				 * I tried there was an issue in GE when using both a layer from
-				 * maptiler and a kml areas.
-				 */
-				Coordinate coordinate = new Coordinate(point.getLon(),
-						point.getLat(), FeatureMap.METERS_ABOVE_GROUND);
-				coordinates.add(coordinate);
-			}
-			// ensure first coord is the same as last as specified by kml
-			if (!coordinates.get(0).equals(
-					coordinates.get(coordinates.size() - 1))) {
-				coordinates.add(coordinates.get(0).clone());
-			}
-			updateCoordinateLabel();
-			map.showFeature(polygon);
-			draw.setVisible(true);
-			draw.setCaption("Redraw");
-		} else {
-			System.err.println("Non supported vector drawn:"
-					+ vector.getClass());
-		}
-	}
+    private void drawFeature() {
+        map.drawFeature(this);
+        draw.setVisible(false);
+        label.setValue("Drawing...");
+    }
+
+    public void drawingDone(Vector vector) {
+        if (vector instanceof Area) {
+            Area area = (Area) vector;
+            coordinates.clear();
+            Point[] points = area.getPoints();
+            for (Point point : points) {
+                /*
+                 * Note that we intentionally place overlays 30 meter above
+                 * ground. This way above base layers in google earth (probably
+                 * bugs if there are lots of altitude differences and only few
+                 * points in the area (or large area).
+                 * 
+                 * TODO check whats up with clamped to ground option. Last time
+                 * I tried there was an issue in GE when using both a layer from
+                 * maptiler and a kml areas.
+                 */
+                Coordinate coordinate = new Coordinate(point.getLon(),
+                        point.getLat(), FeatureMap.METERS_ABOVE_GROUND);
+                coordinates.add(coordinate);
+            }
+            // ensure first coord is the same as last as specified by kml
+            if (!coordinates.get(0).equals(
+                    coordinates.get(coordinates.size() - 1))) {
+                coordinates.add(coordinates.get(0).clone());
+            }
+            updateCoordinateLabel();
+            map.showFeature(polygon, null);
+            draw.setVisible(true);
+            draw.setCaption("Redraw");
+        } else {
+            System.err.println("Non supported vector drawn:"
+                    + vector.getClass());
+        }
+    }
 
 }
